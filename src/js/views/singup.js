1d2f9ca6ec1c8d8c252b27363007591f5d9077d4
Gilbert Marín <daviddlioncourt@gmail.com>
2021-04-14 13:50:28 +0000

    Login

--- src/js/views/singup.js
+++ src/js/views/singup.js
@@ -2,12 +2,24 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
-import { Container, Form, Input, Button } from "react-bootstrap";
+import { Container, Form, Input, Button, Row, Col, FormGroup } from "react-bootstrap";
 
 import "../../styles/demo.scss";
 
 export const SingUp = () => {
 	const { store, actions } = useContext(Context);
 
-	return <Container />;
+	return (
+		<Container>
+			<Row class="justify-content-center pt-5 mt-5">
+				<Col>
+					<Form>
+						<FormGroup>
+							<h1>Sing In</h1>
+						</FormGroup>
+					</Form>
+				</Col>
+			</Row>
+		</Container>
+	);
 };
